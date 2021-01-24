4f0b51aeeeefaff0eafabe121a964dbbd66ee99e
ZhongYu <zhongyu.vip@gmail.com>
2021-01-24 11:45:16 +0800

    更新mvc配置

---
+++ src/main/kotlin/com/auium/config/MvcConfiguration.kt
@@ -0,0 +1,20 @@
+package com.auium.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
+import org.springframework.http.converter.HttpMessageConverter
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport
+import org.zalando.problem.jackson.ProblemModule
+
+@Configuration
+@Import(ExceptionHandling::class)
+class MvcConfiguration : WebMvcConfigurationSupport() {
+
+    override fun configureMessageConverters(converters: MutableList<HttpMessageConverter<*>>) {
+        converters.clear()
+        converters.add(MappingJackson2HttpMessageConverter(ObjectMapper().registerModule(ProblemModule())))
+    }
+
+}
