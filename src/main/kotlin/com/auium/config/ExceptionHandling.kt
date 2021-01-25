a45512438710f3e4b622c54b0802c7cb2d888da3
zhongyu <yuzhong@shein.com>
2021-01-25 16:24:55 +0800

    更新用户名或密码错误

--- src/main/kotlin/com/auium/config/ExceptionHandling.kt
+++ src/main/kotlin/com/auium/config/ExceptionHandling.kt
@@ -1,9 +1,23 @@
 package com.auium.config
 
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.AuthenticationException
 import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.context.request.NativeWebRequest
+import org.zalando.problem.Problem
+import org.zalando.problem.Status
 import org.zalando.problem.spring.web.advice.ProblemHandling
 import org.zalando.problem.spring.web.advice.security.SecurityAdviceTrait
 
 @ControllerAdvice
 class ExceptionHandling : ProblemHandling, SecurityAdviceTrait {
+
+    override fun handleAuthentication(ex: AuthenticationException, request: NativeWebRequest): ResponseEntity<Problem> {
+        val problem: Problem = Problem.builder()
+            .withStatus(Status.OK)
+            .withTitle("Authentication failed")
+            .withDetail(ex.localizedMessage).build()
+        return create(ex, problem, request)
+    }
+
 }
